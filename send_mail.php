210d2b6713c666fa3bd08d3cc84ba252e54cc8f3
Sarnava Mohanta <sarnavamohanta2001@gmail.com>
2024-06-02 10:02:03 +0530

    initial commit

---
+++ send_mail.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = 'your-email@gmail.com'; // Replace with your Gmail address
+
+    $subject = 'Contact Form Submission: ' . $subject;
+
+    $message_body = "Name: $name\n";
+    $message_body .= "Email: $email\n\n";
+    $message_body .= "Message:\n$message";
+
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    if (mail($to, $subject, $message_body, $headers)) {
+        echo 'Message sent successfully.';
+    } else {
+        echo 'Error sending message.';
+    }
+}
+?>
